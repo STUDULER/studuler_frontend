802c253a00cb2314d57bfa323a00dbadf6f069be
2020026635 <dnwjd99@hanyang.ac.kr>
2024-10-04 23:36:58 +0900

    Feat: 간단한 역할 선택 페이지

---
+++ lib/common/page/role_selection_page.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+import '../section/yellow_background.dart';
+import 'login_page.dart';
+
+class RoleSelectionPage extends StatelessWidget {
+  const RoleSelectionPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: YellowBackground(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Spacer(
+                flex: 2,
+              ),
+              Text(
+                "STUDULER",
+                style: Theme.of(context).textTheme.displayLarge,
+              ),
+              const Text("당신의 역할을 선택해주세요"),
+              const Spacer(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    children: [
+                      GestureDetector(
+                        onTap: () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => LoginPage(),
+                          ),
+                        ),
+                        child: Container(
+                          width: MediaQuery.sizeOf(context).width / 2.5,
+                          height: MediaQuery.sizeOf(context).width / 2.5,
+                          decoration: const BoxDecoration(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(128)),
+                            color: Colors.amber,
+                          ),
+                        ),
+                      ),
+                      const Text("선생님"),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      GestureDetector(
+                        onTap: () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => LoginPage(),
+                          ),
+                        ),
+                        child: Container(
+                          width: MediaQuery.sizeOf(context).width / 2.5,
+                          height: MediaQuery.sizeOf(context).width / 2.5,
+                          decoration: const BoxDecoration(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(128)),
+                            color: Colors.amber,
+                          ),
+                        ),
+                      ),
+                      const Text("학부모"),
+                    ],
+                  ),
+                ],
+              ),
+              const Spacer(
+                flex: 3,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
