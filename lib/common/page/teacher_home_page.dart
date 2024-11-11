5e66fed20391dbe2665e6d2621be157b64349f12
2020026635 <dnwjd99@hanyang.ac.kr>
2024-11-11 18:34:04 +0900

    백그라운드가 버튼을 인자로 받는다

--- lib/common/page/teacher_home_page.dart
+++ lib/common/page/teacher_home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:card_swiper/card_swiper.dart';
+import '../../teacher/page/add_class_page.dart';
 import '../widget/class_info_card.dart';
 import '../section/class_info_item.dart';
 import '../widget/background.dart';
@@ -23,30 +24,33 @@ class _TeacherHomePageState extends State<TeacherHomePage> {
         'title': '대치동 수학 과외',
         'code': 'REK45J2F',
         'completionRate': 0.75,
-        'themeColor': Color(0xFFB5C18E), // 초기 테마 색상 추가
+        'themeColor': const Color(0xFFB5C18E), // 초기 테마 색상 추가
         'infoItems': [
-          ClassInfoItem(icon: Icons.person, title: '학생 이름', value: '홍길동'),
-          ClassInfoItem(icon: Icons.access_time, title: '회당 시간', value: '3시간'),
-          ClassInfoItem(icon: Icons.calendar_today, title: '요일', value: '월/수/금'),
-          ClassInfoItem(icon: Icons.payment, title: '정산 방법', value: '선불'),
-          ClassInfoItem(icon: Icons.attach_money, title: '시급', value: '12500원'),
-          ClassInfoItem(icon: Icons.repeat, title: '수업 횟수', value: '8회'),
-          ClassInfoItem(icon: Icons.calendar_today, title: '다음 정산일', value: '9월 18일'),
+          const ClassInfoItem(icon: Icons.person, title: '학생 이름', value: '홍길동'),
+          const ClassInfoItem(icon: Icons.access_time, title: '회당 시간', value: '3시간'),
+          const ClassInfoItem(
+              icon: Icons.calendar_today, title: '요일', value: '월/수/금'),
+          const ClassInfoItem(icon: Icons.payment, title: '정산 방법', value: '선불'),
+          const ClassInfoItem(icon: Icons.attach_money, title: '시급', value: '12500원'),
+          const ClassInfoItem(icon: Icons.repeat, title: '수업 횟수', value: '8회'),
+          const ClassInfoItem(
+              icon: Icons.calendar_today, title: '다음 정산일', value: '9월 18일'),
         ],
       },
       {
         'title': '서초동 영어 과외',
         'code': 'ENG12345',
         'completionRate': 0.5,
-        'themeColor': Color(0xFFFCCFCF), // 초기 테마 색상 추가
+        'themeColor': const Color(0xFFFCCFCF), // 초기 테마 색상 추가
         'infoItems': [
-          ClassInfoItem(icon: Icons.person, title: '학생 이름', value: '이몽룡'),
-          ClassInfoItem(icon: Icons.access_time, title: '회당 시간', value: '2시간'),
-          ClassInfoItem(icon: Icons.calendar_today, title: '요일', value: '화/목'),
-          ClassInfoItem(icon: Icons.payment, title: '정산 방법', value: '후불'),
-          ClassInfoItem(icon: Icons.attach_money, title: '시급', value: '15000원'),
-          ClassInfoItem(icon: Icons.repeat, title: '수업 횟수', value: '5회'),
-          ClassInfoItem(icon: Icons.calendar_today, title: '다음 정산일', value: '10월 10일'),
+          const ClassInfoItem(icon: Icons.person, title: '학생 이름', value: '이몽룡'),
+          const ClassInfoItem(icon: Icons.access_time, title: '회당 시간', value: '2시간'),
+          const ClassInfoItem(icon: Icons.calendar_today, title: '요일', value: '화/목'),
+          const ClassInfoItem(icon: Icons.payment, title: '정산 방법', value: '후불'),
+          const ClassInfoItem(icon: Icons.attach_money, title: '시급', value: '15000원'),
+          const ClassInfoItem(icon: Icons.repeat, title: '수업 횟수', value: '5회'),
+          const ClassInfoItem(
+              icon: Icons.calendar_today, title: '다음 정산일', value: '10월 10일'),
         ],
       },
     ];
@@ -57,7 +61,25 @@ class _TeacherHomePageState extends State<TeacherHomePage> {
     return Scaffold(
       body: Stack(
         children: [
-          const Background(),
+          Background(
+            iconActionButtons: [
+              IconButton(
+                icon: const Icon(Icons.add, color: Colors.black),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const AddClassPage(),
+                    ),
+                  );
+                },
+              ),
+              IconButton(
+                icon: const Icon(Icons.menu, color: Colors.black),
+                onPressed: () {},
+              ),
+            ],
+          ),
           Column(
             children: [
               const SizedBox(height: 140),
@@ -68,10 +90,14 @@ class _TeacherHomePageState extends State<TeacherHomePage> {
                   return AnimatedContainer(
                     duration: const Duration(milliseconds: 300),
                     margin: const EdgeInsets.symmetric(horizontal: 4.0),
-                    width: currentIndex == index ? 20.0 : 8.0, // 현재 카드의 인덱스일 때는 길게 표시
+                    width: currentIndex == index
+                        ? 20.0
+                        : 8.0, // 현재 카드의 인덱스일 때는 길게 표시
                     height: 8.0,
                     decoration: BoxDecoration(
-                      color: currentIndex == index ? Colors.grey[600] : Colors.grey[400],
+                      color: currentIndex == index
+                          ? Colors.grey[600]
+                          : Colors.grey[400],
                       borderRadius: BorderRadius.circular(4.0),
                     ),
                   );
@@ -90,11 +116,13 @@ class _TeacherHomePageState extends State<TeacherHomePage> {
                       completionRate: classData[index]['completionRate'],
                       themeColor: classData[index]['themeColor'], // 테마 색상 전달
                       infoItems: classData[index]['infoItems'],
-                      onUpdate: (updatedTitle, updatedInfoItems, updatedThemeColor) {
+                      onUpdate:
+                          (updatedTitle, updatedInfoItems, updatedThemeColor) {
                         setState(() {
                           classData[index]['title'] = updatedTitle;
                           classData[index]['infoItems'] = updatedInfoItems;
-                          classData[index]['themeColor'] = updatedThemeColor; // 테마 색상 업데이트
+                          classData[index]['themeColor'] =
+                              updatedThemeColor; // 테마 색상 업데이트
                         });
                       },
                     );
