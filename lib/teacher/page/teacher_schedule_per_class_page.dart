989247e8aeba5a18e1e628c3713f4b6438bbe93d
2020026635 <dnwjd99@hanyang.ac.kr>
2025-01-17 20:50:51 +0900

    피드백 불러오기

--- lib/teacher/page/teacher_schedule_per_class_page.dart
+++ lib/teacher/page/teacher_schedule_per_class_page.dart
@@ -49,12 +49,10 @@ class _TeacherSchedulePerClassPageState
     fetchClassDays(selectedDate.value);
     super.initState();
     selectedDate.addListener(() async {
-      if (weekMode.value == true) {
-        classFeedback.value = await httpService.fetchClassFeedback(
-          classId: widget.classId,
-          date: selectedDate.value,
-        );
-      }
+      classFeedback.value = await httpService.fetchClassFeedback(
+        classId: widget.classId,
+        date: selectedDate.value,
+      );
       fetchClassDays(selectedDate.value);
     });
 
